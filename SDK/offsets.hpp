096d0d1f4138f759086d5b6c4891420d9eff2888
ro0ti <81922230+ro0ti@users.noreply.github.com>
2023-10-06 22:45:42 -0400

    update

--- SDK/offsets.hpp
+++ SDK/offsets.hpp
@@ -3,18 +3,18 @@
 #include <cstddef>
 
 namespace client_dll {
-    constexpr std::ptrdiff_t entityList = 0x178D8E8;
-    constexpr std::ptrdiff_t globalVars = 0x1690D18;
-    constexpr std::ptrdiff_t interfaceLinkList = 0x1973BA8;
-    constexpr std::ptrdiff_t localPlayerController = 0x17DC158;
-    constexpr std::ptrdiff_t localPlayerPawn = 0x187AC48;
-    constexpr std::ptrdiff_t plantedC4 = 0x1880DE0;
-    constexpr std::ptrdiff_t viewAngles = 0x18DA700;
-    constexpr std::ptrdiff_t viewMatrix = 0x187B730;
+    constexpr std::ptrdiff_t entityList = 0x17888D8;
+    constexpr std::ptrdiff_t globalVars = 0x168BCE8;
+    constexpr std::ptrdiff_t interfaceLinkList = 0x196EC98;
+    constexpr std::ptrdiff_t localPlayerController = 0x17D7158;
+    constexpr std::ptrdiff_t localPlayerPawn = 0x1875C48;
+    constexpr std::ptrdiff_t plantedC4 = 0x187B9E0;
+    constexpr std::ptrdiff_t viewAngles = 0x18D5700;
+    constexpr std::ptrdiff_t viewMatrix = 0x1876730;
 }
 
 namespace engine2_dll {
-    constexpr std::ptrdiff_t networkGameClient = 0x48BAB0;
+    constexpr std::ptrdiff_t networkGameClient = 0x485AB0;
     constexpr std::ptrdiff_t networkGameClient_maxClients = 0x250;
     constexpr std::ptrdiff_t networkGameClient_signOnState = 0x240;
 }
--- SDK/offsets.rs
+++ SDK/offsets.rs
@@ -1,18 +1,18 @@
 #![allow(non_snake_case, non_upper_case_globals)]
 
 pub mod client_dll {
-    pub const entityList: usize = 0x178D8E8;
-    pub const globalVars: usize = 0x1690D18;
-    pub const interfaceLinkList: usize = 0x1973BA8;
-    pub const localPlayerController: usize = 0x17DC158;
-    pub const localPlayerPawn: usize = 0x187AC48;
-    pub const plantedC4: usize = 0x1880DE0;
-    pub const viewAngles: usize = 0x18DA700;
-    pub const viewMatrix: usize = 0x187B730;
+    pub const entityList: usize = 0x17888D8;
+    pub const globalVars: usize = 0x168BCE8;
+    pub const interfaceLinkList: usize = 0x196EC98;
+    pub const localPlayerController: usize = 0x17D7158;
+    pub const localPlayerPawn: usize = 0x1875C48;
+    pub const plantedC4: usize = 0x187B9E0;
+    pub const viewAngles: usize = 0x18D5700;
+    pub const viewMatrix: usize = 0x1876730;
 }
 
 pub mod engine2_dll {
-    pub const networkGameClient: usize = 0x48BAB0;
+    pub const networkGameClient: usize = 0x485AB0;
     pub const networkGameClient_maxClients: usize = 0x250;
     pub const networkGameClient_signOnState: usize = 0x240;
 }
